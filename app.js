919f24d59223def8b3fe36e3a83fc19b750fcd79
mobilemelody <melody.reebs@gmail.com>
2017-06-01 22:21:49 -0700

    added todo list and todo presentation components

--- app.js
+++ app.js
@@ -14,3 +14,30 @@ const TodoForm = ({addTodo}) => {
         </div>
     );
 };
+
+const Todo = ({todo, remove}) => {
+    // Each todo
+    return (
+        <li onClick(remove(todo.id)>
+            {todo.text}
+        </li>
+    );
+};
+
+const TodoList = ({todos, remove}) => {
+    // Map through the todos
+    const todoNode = todos.map((todo) => {
+       return (<Todo todo={todo} key={todo.id} remove={remove} />); 
+    });
+    return (<ul>{todoNode}</ul>);
+};
+
+const Title = () => {
+    return (
+        <div>
+            <div>
+                <h1>to-do</h1>
+            </div>
+        </div>
+    );
+};
