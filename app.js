eb39dbcfa8d1dfdc5a5956e22dfaca547c0f838b
mobilemelody <melody.reebs@gmail.com>
2017-06-04 11:22:10 -0700

    setup React environment

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
 const TodoForm = ({addTodo}) => {
     // Input tracker
     let input;
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+    template: __dirname + '/index.html',
+    filename: 'index.html',
+    inject: 'body'
+});
+
+module.exports = {
+    entry: __dirname + '/app.js',
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            }
+        ]
+    },
+    output: {
+        filename: 'bundle.js',
+        path: __dirname + '/build'
+    },
+    plugins: [HtmlWebpackPluginConfig]
+};
